9a55e86f8d552901cc95b14cbfe38922e63cd66b
level3tjg <t.grantham14@gmail.com>
2022-03-20 13:20:00 -0400

    patch ipa with perl

---
+++ AVFAudio/AVFAudio.h
@@ -0,0 +1,3 @@
+// Umbrella header for AVFAudio.
+// Add import lines for each public header, like this: #import <AVFAudio/XXXAwesomeClass.h>
+// Don’t forget to also add them to AVFAudio_PUBLIC_HEADERS in your Makefile!
--- patch_ipa.sh
+++ patch_ipa.sh
@@ -1,7 +1,8 @@
 unzip Alien_Isolation.ipa
 rm Alien_Isolation.ipa
-install_name_tool -change "/System/Library/Frameworks/AVFAudio.framework/AVFAudio" "/System/Library/Frameworks/AVFoundation.framework/Frameworks/AVFAudio.framework/AVFAudio" Payload/Alien\ Isolation.app/Alien\ Isolation
-install_name_tool -change "/System/Library/Frameworks/GameController.framework/GameController" "@rpath/GameController.framework/GameController" Payload/Alien\ Isolation.app/Alien\ Isolation
-defaults write $(pwd)/Payload/Alien\ Isolation.app/Info MinimumOSVersion -string "13.0"
+mkdir Payload/Alien\ Isolation.app/Frameworks
+perl -pi -e 's/\/System\/Library\/Frameworks\/AVFAudio.framework\/AVFAudio/\@rpath\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/AVFAudio.framework\/AVFAudio/g' Payload/Alien\ Isolation.app/Alien\ Isolation
+perl -pi -e 's/\/System\/Library\/Frameworks\/GameController.framework\/GameController/\@rpath\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/GameController.framework\/GameController/g' Payload/Alien\ Isolation.app/Alien\ Isolation
+perl -pi -e 's/\<string\>14.8\<\/string\>/\<string\>13.0\<\/string\>/g' Payload/Alien\ Isolation.app/Info.plist
 zip -r Alien_Isolation.ipa Payload
 rm -rf Payload
